2304fa0f0b96754a73e3a45d0d25b42a3e68f577
Ashley <ashley.j.stewart@live.com>
2015-04-09 18:15:16 +1200

    Next update

---
+++ ConnectController.js
@@ -0,0 +1,4 @@
+$(document).ready(function() {
+  var grid = new Grid(25,48);
+  grid.renderGrid();
+});
---
+++ ConnectModel.js
@@ -0,0 +1,20 @@
+//A constructor function here
+
+function Grid (height, width) {
+  this.width = width;
+  this.height = height;
+}
+
+
+
+//A prototype which renders the grid
+Grid.prototype.renderGrid = function() {
+  for (var i=0; i < this.height; i++) {
+    var row = $("<div class='row'></div>");
+    $( '#grid').append(row);
+    for (var j=0;j < this.width; j++) {
+      var cell = $("<div class='cell'></div>");
+      row.append(cell);
+    }
+  }
+};
